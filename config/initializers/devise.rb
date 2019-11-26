d63ebe7576bd8a1ad5537c429a53abcb9414ca2f
Khaled Awad <khaledbnmohamed@hotmail.com>
2019-11-26 16:52:57 +0200

    expsosing error status without devise redirection

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,15 +254,10 @@ Devise.setup do |config|
   # config.sign_out_all_scopes = true
 
   # ==> Navigation configuration
-  # Lists the formats that should be treated as navigational. Formats like
-  # :html, should redirect to the sign in page when the user does not have
-  # access, but formats like :xml or :json, should return 401.
-  #
-  # If you have any extra navigational formats, like :iphone or :mobile, you
-  # should add them to the navigational formats lists.
-  #
-  # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  #It will prevent devise from using flash messages 
+  #which are not present in Rails api mode.
+
+  config.navigational_formats = []
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
